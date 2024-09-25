b9d7fa7e89dd774cf9d99012ff927c4ab0356df3
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-09-24 23:41:41 -0400

    feat: pokemon paginated list Fixes Pokemon List #6

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,37 @@
-import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-import React from "react";
+import { Suspense } from "react";
 
-export default async function DashboardPage() {
+import PokemonList from "@/components/pokemon-list";
+
+import { auth } from "@/auth";
+import SearchInput from "@/components/search-input";
+import { LoaderCircle } from "lucide-react";
+
+export default async function DashboardPage({
+  searchParams,
+}: {
+  searchParams?: {
+    q?: string;
+    offset?: string;
+  };
+}) {
   const session = await auth();
   if (!session) redirect("/");
-  return <div>DashboardPage</div>;
+
+  const q = searchParams?.q || "";
+  const offset = Number(searchParams?.offset) || 1;
+
+  return (
+    <main className="flex flex-col justify-between h-screen mx-auto w-full">
+      <header className="h-12 w-full p-1">
+        <SearchInput />
+      </header>
+      <Suspense
+        key={offset + q}
+        fallback={<LoaderCircle className="h-12  animate-spin grow w-full" />}
+      >
+        <PokemonList offset={offset} q={q} />
+      </Suspense>
+    </main>
+  );
 }
